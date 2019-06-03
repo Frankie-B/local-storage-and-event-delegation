87ae32c0cb0d9076211ba79be41c4f5c7d878c72
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-03 16:46:57 +0300

    Add event delgation

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items')) || [];
+const items = JSON.parse(localStorage.getItem('items')) || []; // On page load checks local storage.
 
 function addItem(e) {
   e.preventDefault();
@@ -14,7 +14,7 @@ function addItem(e) {
 
   items.push(item); // Creates an object from the data input by the user.
   populateList(items, itemsList);
-  localStorage.setItem('items', JSON.stringify(items));
+  localStorage.setItem('items', JSON.stringify(items)); // Puts items into local storage  as a string.
   this.reset(); // Resets form input field
 }
 
@@ -33,5 +33,17 @@ function populateList(plates = [], platesList) {
     })
     .join('');
 }
+
+// The code below will persist the toggle of items
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // Will skip this unless it is an input.
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
 populateList(items, itemsList);
