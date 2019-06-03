6ef14b37615893cda87c573940ade4b1dac1a0d6
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-03 14:32:17 +0300

    Create Objects from user input

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 // jshint esversion: 6
 
 const addItems = document.querySelector('.add-items');
-  const itemsList = document.querySelector('.plates');
-  const items = [];
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text,
+    done: false,
+  };
+  this.reset();
+}
+addItems.addEventListener('submit', addItem);
